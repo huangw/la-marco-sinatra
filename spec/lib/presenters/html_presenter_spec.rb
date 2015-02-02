1794792a14e4c2b474c3f805bd9a706972e2e3a8
Songcy <songcy@7lime.com>
2015-02-02 14:19:58 +0800

    modify the html_presenter spec

--- spec/lib/presenters/html_presenter_spec.rb
+++ spec/lib/presenters/html_presenter_spec.rb
@@ -10,26 +10,22 @@ class MockEmail
 end
 
 describe HtmlPresenter do
-  it 'default slim template' do
-    em = MockEmail.new
+  subject(:em) { MockEmail.new }
 
+  it 'default slim template' do
     expect(em.to_html).to eq("<div class=\"default\">mock_email</div>")
     expect(em.to_html(:list)).to eq("<div class=\"list\">mock_email</div>")
   end
 
   it 'not exits locale' do
-    em = MockEmail.new
     expect{ em.to_html(:default, locales: ['fr']) }.to raise_error(Errno::ENOENT)
   end
 
   it 'not exits type' do
-    em = MockEmail.new
     expect{ em.to_html(:test) }.to raise_error(Errno::ENOENT)
   end
 
   it 'given a list locales' do
-    em = MockEmail.new
-
     I18n.locale = :en
     expect(em.to_html(:default, locales: ['zh', 'en', 'ja'])).to eq("<div class=\"default_en\">mock_email</div>")
 
@@ -38,8 +34,6 @@ describe HtmlPresenter do
   end
 
   it 'given a list locales' do
-    em = MockEmail.new
-
     expect(em.to_html(:dat, name: 'test')).to eq("<div class=\"dat\">test</div>")
   end
 end
