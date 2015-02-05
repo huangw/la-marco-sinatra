c57e9441b925a62516845f83da98e82633283f80
Songcy <songcy@7lime.com>
2015-02-05 17:07:47 +0800

    require the helper

--- config/initializers/routes.rb
+++ config/initializers/routes.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'route'
 require_relative 'web_exceptions'
 
+deep_require root_join('lib/helpers')
+deep_require root_join('app/helpers')
 deep_require root_join('lib/rack')
 deep_require root_join('lib/controllers')
 deep_require root_join('app/pages')
