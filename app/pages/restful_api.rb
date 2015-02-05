852ec335a77cf20ced3bab013a379bd6216fd899
Songcy <songcy@7lime.com>
2015-02-05 12:38:01 +0800

    modify the restful api

--- app/pages/restful_api.rb
+++ app/pages/restful_api.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 # require_relative 'la_sinatra_web'
+# require_relative '../../config/initializers/web_exceptions'
 
 # Error for test application
 class AuthenticationError < ServerError
