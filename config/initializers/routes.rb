e87f82f3c4148e85f2439a01ac7d95e4af6c29e0
Songcy <songcy@7lime.com>
2015-02-05 12:14:09 +0800

    modify the stackfile

--- config/initializers/routes.rb
+++ config/initializers/routes.rb
@@ -1,30 +1,6 @@
 require 'sinatra/base'
 require 'route'
 
-# Namespace for exceptions that known by the application
-class ServerError < RuntimeError
-  def status
-    500
-  end
-end
-
-# Generic
-class RequestError < ArgumentError
-  def status
-    400
-  end
-end
-
-# Validation errors occured in different layers
-class ValidationError < RequestError; end
-
-# Not found, etc.
-class RouteError < RequestError
-  def status
-    404
-  end
-end
-
 deep_require root_join('lib/rack')
 deep_require root_join('lib/controllers')
 deep_require root_join('app/pages')
---
+++ config/initializers/web_exceptions.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+# namespace for exceptions that known by the application
+class ServerError < RuntimeError
+  def status
+    500
+  end
+end
+
+# namespace for user input validation errors
+class RequestError < ArgumentError
+  def status
+    400
+  end
+end
+
+# validation errors occured in different layers
+class ValidationError < RequestError; end
+
+# not found, etc.
+class RouteError < RequestError
+  def status
+    404
+  end
+end
