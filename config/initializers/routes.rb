5d1a94075eacc6b31efe8cc76ff5457ffa39aaa3
Huang Wei <huangw@7lime.com>
2015-02-07 11:59:09 +0800

    add api controller file generator

--- config/initializers/routes.rb
+++ config/initializers/routes.rb
@@ -3,7 +3,8 @@ require 'route'
 require_relative 'web_exceptions'
 
 deep_require root_join('lib/helpers')
-deep_require root_join('app/helpers')
 deep_require root_join('lib/rack')
 deep_require root_join('lib/controllers')
+
+deep_require root_join('app/helpers')
 deep_require root_join('app/pages')
