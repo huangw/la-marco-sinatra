e1e10529123f2f05c357df19887de90dc2cb3c50
Huang Wei <huangw@7lime.com>
2015-11-24 10:28:26 +0800

    [progress] update progress to 25

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # La-Marco-Sinatra: Sinatra Based Web Framework
 
 > 注意：`sinatra`如果出现“undefined method `join` for #<String> ...”错误，参照`doc/monkey_patch.html`)文件直接修改`sinatra`源码。
+
+## 在不同环境下启动Puma服务
+
+自行开发了`Route`机制，方便独立app（如bluemoon）的无缝加载或移除。同时开发了基于
+`Route`的path helper，实现对面包屑的程序化管理。
+
+详细参照参照[Route机制](route.html)文档
+
+重新使用`foreman`，用于开发和测试时启动各种服务器包括`puma`。启动`puma`时记录`pid`
+文件到本地`tmp`文件夹下，可通过`rake s`（`rake server`的别名）快速重启。
---
+++ lib/route.rb
@@ -0,0 +1,55 @@
+require 'active_support/inflector/inflections' # :underscore
+
+# global route table
+class Route
+  class << self
+    def table
+      @table ||= {} # ControllerClass => '/maunt/point'
+    end
+
+    # Reversed mounting table:
+    # '/maunt/point' => ControllerClass
+    def all
+      Hash[table.map { |k, rt| [rt, k.new] }]
+    end
+
+    # add class to specific route
+    def []=(app_class, route)
+      table[app_class] = route.to_s
+    end
+
+    # if key is a string, return class, otherwise return the path
+    def [](key)
+      return table[key] if key.is_a?(Class)
+      return all[key].class if key.is_a?(String)
+      nil
+    end
+
+    # add class to default route
+    def mount(app_class, route = nil)
+      route ||= default_path(app_class).tr('_', '/')
+      table[app_class] = route
+    end
+    alias_method :'<<', :mount
+
+    # To helper receive model as inputs:
+    def to(app_class, *parts)
+      prefix = app_class.is_a?(Class) ? @table[app_class] : app_class.to_s
+      fail "unknown controller #{app_class}" if prefix.nil?
+      return prefix unless parts && parts.size > 0
+
+      if parts.last.is_a?(Hash)
+        pam = '?' + parts.pop.map { |k, v| k.to_s + '=' + v.to_s }.join('&')
+      end
+      pam ||= ''
+      pas = parts.map { |k| k.to_s.underscore.tr('_', '/') }
+
+      File.join(prefix, *pas) + pam
+    end
+
+    def default_path(app_class)
+      app_class.to_s.underscore.sub(/_(api|page|controller)$/, '')
+        .sub(/^\/*/, '/').pluralize
+    end
+  end
+end
