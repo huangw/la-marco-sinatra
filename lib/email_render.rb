dbc0d18e78ddfae68e58294036b9a514a2d46a7e
Huang Wei <huangw@7lime.com>
2015-11-28 21:06:59 +0800

    [progress] update progress to 30

--- lib/email_render.rb
+++ lib/email_render.rb
@@ -3,6 +3,7 @@
 # vi: foldlevel=1
 # created at: 2015-11-27
 require 'active_support/concern'
+require 'background/job'
 
 # Find email template file for varies locales and formats,
 # parse custom header and render email body
@@ -10,23 +11,24 @@ module EmailRender
   extend ActiveSupport::Concern
 
   included do
+    attr_accessor :headers, :bodies
+    attr_writer :sender
+
+    include Background::Job
+
     include Mongoid::FieldCandy::EmailField
     email_field :to, required: true
 
     field :lc, as: :locale, type: Symbol, default: -> { default_locale }
-    field :se, as: :send_error, type: String
+    field :o_at, as: :opened_at, type: Time
 
     validate do
       errors.add :locale,
                  :inclusion if localized? && !available_locales.include?(lc)
     end
 
-    # calculate locale from locales settings
-    def default_locale
-      return nil unless localized?
-      u_lc = I18n.locale.to_sym
-      available_locales.include?(u_lc) ? u_lc : available_locales[0]
-    end
+    # Override those methods in subclasses
+    # ---------------------------------------
 
     def template_dir
       'app/views/'
@@ -44,14 +46,76 @@ module EmailRender
       [:zh, :ja, :en]
     end
 
+    # find template
+    # ---------------
+
     def localized?
       available_locales && available_locales.size > 0
     end
 
+    # calculate locale from locales settings
+    def default_locale
+      return nil unless localized?
+      u_lc = I18n.locale.to_sym
+      available_locales.include?(u_lc) ? u_lc : available_locales[0]
+    end
+
+    def default_format
+      available_formats.first
+    end
+
     def template_name(format = nil, locale = nil)
       template_file = template_basename
-      template_file += '.' + format unless format.nil?
+
+      locale ||= default_locale if localized? # may has a locale
+      template_file += ".#{locale}" if locale
+
+      format ||= default_format
+      template_file += ".#{format}" # always has a format
+
       File.join(template_dir, template_file)
     end
+
+    # parse the template
+    # ---------------------
+
+    def valid_fields
+      %w(to cc bcc subject from sender reply-to return-path inline)
+    end
+
+    def parse(extra_headers = {})
+      @bodies = {}
+      available_formats.each do |fmt|
+        body = Liquid::Template.parse(
+          File.open(template_file(fmt), 'r:utf-8').read).render(to_hash)
+
+        if @headers.nil?
+          head, body = body.split("\n\n", 2)
+          setup_header(YAML.load(head).merge(extra_headers.stringify_keys))
+        end
+        @bodies[fmt] = body
+      end
+    end
+
+    def setup_header(dat)
+      @headers = {}
+      dat.each do |key, val|
+        fail "invalid field #{key}" unless valid_fields.include?(key.to_s)
+        @headers[key.to_sym] = val unless val.nil?
+      end
+    end
+
+    # delivery the email
+    # ---------------------
+
+    # default sender defined at `config/settings/email_sender_settings`
+    def sender
+      @sender ||= default_sender
+    end
+
+    def process!
+      sender.deliver!(@headers, @bodies)
+    end
+    alias_method :deliver!, :process!
   end
 end
--- spec/lib/email_render_spec.rb
+++ spec/lib/email_render_spec.rb
@@ -12,6 +12,12 @@ class MockEmailLessLocale < MockEmail
   end
 end
 
+class MockEmailWithoutLocale < MockEmail
+  def available_locales
+    []
+  end
+end
+
 describe EmailRender do
   subject(:email) { MockEmail.new to: 'huangw@pe-po.com' }
   describe '#to' do
@@ -32,6 +38,24 @@ describe EmailRender do
     it 'can not be set to unsupported locale' do
       email.locale = :it
       expect(email).to have_validate_error(:inclusion).on(:locale)
+    end
+  end
+
+  describe '#template_name' do
+    it 'return the template for render the email model' do
+      expect(email.template_name).to eq('app/views/mock_email.en.txt')
+      expect(email.template_name(:html)).to eq('app/views/mock_email.en.html')
+      expect(email.template_name(:html, :fr)).to eq('app/views/mock_email.fr.html')
+    end
+
+    it 'return the first locale as the default locale' do
+      expect(MockEmailLessLocale.new.template_name).to eq('app/views/mock_email_less_locale.de.txt')
+      expect(MockEmailLessLocale.new.template_name(:html)).to eq('app/views/mock_email_less_locale.de.html')
+    end
+
+    it 'supports template without locale' do
+      expect(MockEmailWithoutLocale.new.template_name).to eq('app/views/mock_email_without_locale.txt')
+      expect(MockEmailWithoutLocale.new.template_name(:html)).to eq('app/views/mock_email_without_locale.html')
     end
   end
 end
