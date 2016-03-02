604fc488600c3551f61d3d642fcf45f66c9189f1
Songcy <songcy@7lime.com>
2016-03-02 11:11:33 +0800

    modify the assets helper

--- lib/helpers/assets_helper.rb
+++ lib/helpers/assets_helper.rb
@@ -12,38 +12,30 @@ module AssetsHelper
     end.join('')
   end
 
+  def img_url(obj, opts = {})
+    return obj.url unless obj.is_a?(String)
+    obj =~ /^\// ? obj : File.join(AssetSettings.get.img_url_prefix, obj)
+  rescue
+    default_img_url(opts[:suffix], opts[:type])
+  end
+
   def default_img_url(suffix = 'o', type = 'Image')
     File.join(AssetSettings.get.img_url_prefix,
               'defaults',
               "#{type.to_s.underscore}-#{suffix}.png")
   end
 
-  def default_img_alt(obj)
-    if obj.is_a? Image
-      obj.oname
-    elsif obj.is_a? String
-      obj.split(/\//).last
-    else
-      ftt(:img_not_found)
-    end
-  end
-
-  def img_url(obj, opts = {})
+  def img_tag(obj, opts = {})
     opts[:suffix] ||= 'o'
     opts[:type] ||= 'Image'
 
-    if obj.is_a? Image
-      obj.url(opts[:suffix])
-    elsif obj.is_a? String
-      File.join(AssetSettings.get.img_url_prefix, obj)
-    else
-      default_img_url(opts[:suffix], opts[:type])
-    end
-  end
-
-  def img_tag(obj, opts = {})
     opts['src'] = img_url(obj, opts)
-    opts['alt'] = default_img_alt(obj)
+
+    # Set the default alt
+    opts['alt'] ||= obj.split(/\//).last if obj.is_a?(String)
+    opts['alt'] ||= obj.alt if obj.methods.include?(:alt)
+    opts['alt'] ||= ftt(:img_not_found)
+
     attrs = opts.map { |k, v| format('%s="%s"', k, v) }.join(' ')
     format '<image %s />', attrs
   end
