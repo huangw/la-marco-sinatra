a39d9a6799d5adbdc977112fc2135bb3706fcd20
Songcy <songcy@7lime.com>
2016-03-17 10:12:35 +0800

    modify the assets helper

--- lib/helpers/assets_helper.rb
+++ lib/helpers/assets_helper.rb
@@ -12,31 +12,50 @@ module AssetsHelper
     end.join('')
   end
 
-  def img_url(obj, opts = {})
-    return obj.url unless obj.is_a?(String)
-    obj =~ /^\// ? obj : File.join(AssetSettings.get.img_url_prefix, obj)
-  rescue
-    default_img_url(opts[:suffix], opts[:type])
+  def img_tag(obj, opts = {})
+    begin
+      opts[:suffix] ||= 'o'
+
+      # opts type defaults String is 404
+      raise RequestError, :not_a_image unless check_img_obj(obj)
+
+      opts[:type] ||= obj._mt if obj.is_a?(ImageLabel)
+      opts[:type] ||= obj.class
+
+      # opts url
+      opts[:src] ||= img_obj_url(obj, opts)
+
+      # opts alt
+      opts[:alt] ||= obj.split(/\//).last if obj.is_a?(String)
+      opts[:alt] ||= obj.alt
+    rescue
+      opts[:type] ||= 'Image'
+      opts[:src] ||= default_img_url(opts[:suffix], opts[:type])
+      opts[:alt] ||= ftt(:img_not_found)
+    end
+
+    attrs = opts.map { |k, v| format('%s="%s"', k, v) }.join(' ')
+    format '<image %s />', attrs
   end
 
-  def default_img_url(suffix = 'o', type = 'Image')
+  def img_obj_url(obj, opts)
+    if obj.is_a? String
+      obj =~ /^\// ? obj : File.join(AssetSettings.get.img_url_prefix, obj)
+    else
+      obj.resized_url(opts[:suffix])
+    end
+  end
+
+  def default_img_url(suffix, type)
     File.join(AssetSettings.get.img_url_prefix,
               'defaults',
               "#{type.to_s.underscore}-#{suffix}.png")
   end
 
-  def img_tag(obj, opts = {})
-    opts[:suffix] ||= 'o'
-    opts[:type] ||= 'Image'
-
-    opts['src'] = img_url(obj, opts)
-
-    # Set the default alt
-    opts['alt'] ||= obj.split(/\//).last if obj.is_a?(String)
-    opts['alt'] ||= obj.alt if obj.methods.include?(:alt)
-    opts['alt'] ||= ftt(:img_not_found)
-
-    attrs = opts.map { |k, v| format('%s="%s"', k, v) }.join(' ')
-    format '<image %s />', attrs
+  # only accept ImageLabel, Image or String
+  def check_img_obj(obj)
+    return true if obj.is_a?(ImageLabel)
+    return true if obj.is_a?(Image)
+    obj.is_a?(String) && (obj =~ /\./)
   end
 end
